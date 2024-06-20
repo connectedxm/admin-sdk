12feacfdf160d2fe28cc5122eaa7ba4aef02cece
Christian Carneiro <christianhess94@gmail.com>
2024-06-20 08:11:50 -0300

    feat: finish benefits and transations

--- src/mutations/index.ts
+++ src/mutations/index.ts
@@ -2,4 +2,5 @@ export * from "./account";
 export * from "./activity";
 export * from "./advertisement";
 export * from "./announcement";
+export * from "./benefit";
 export * from "./useConnectedMutation";

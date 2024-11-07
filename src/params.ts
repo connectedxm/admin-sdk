f779ca24b20a2b4342dd3b0307eda6ab99961685
joshuackeller <joshuackeller@gmail.com>
2024-11-07 13:10:16 -0700

    make status field in EventPassCreateInputs and EventPassUpdateInputs accept all EventPassStatus values

--- src/params.ts
+++ src/params.ts
@@ -616,7 +616,7 @@ export interface EventPageUpdateInputs {
 }
 
 export interface EventPassCreateInputs {
-  status?: Exclude<EventPassStatus, EventPassStatus.canceled> | null;
+  status?: EventPassStatus | null;
   location?: string | null;
   usedAt?: string | null;
   ticketId?: string | null;
@@ -625,7 +625,7 @@ export interface EventPassCreateInputs {
 }
 
 export interface EventPassUpdateInputs {
-  status?: Exclude<EventPassStatus, EventPassStatus.canceled> | null;
+  status?: EventPassStatus | null;
   location?: string | null;
   usedAt?: string | null;
   ticketId?: string | null;

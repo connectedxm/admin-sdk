6d3d92c048c848e56ab788ee34558169d521e568
Spencer Fund <fund.spencer@gmail.com>
2025-08-15 17:12:26 -0600

    Remove unused session and speaker visibility properties from Event interface for code cleanup.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1019,8 +1019,6 @@ export interface Event extends BaseEvent {
   splitPaymentNetDays: number | null;
   splitPaymentDueDate: string | null;
   publicRegistrants: boolean;
-  sessionsVisible: boolean;
-  speakersVisible: boolean;
   sessionsVisibility: EventAgendaVisibility;
   speakersVisibility: EventAgendaVisibility;
   checkinCode: number | null;

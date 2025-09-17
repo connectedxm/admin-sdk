6a1da437b5718f6771b013269fd97950f1602a04
rob upchurch <rob@connected.events>
2025-09-17 13:29:11 -0600

    Update version to 3.4.0 in package.json and package-lock.json; refactor AuthSession interface and reorganize exports in searchlists and accounts queries for improved structure and clarity.

--- src/queries/logins/index.ts
+++ src/queries/logins/index.ts
@@ -1,4 +1,5 @@
 export * from "./useGetLogin";
 export * from "./useGetLoginAccounts";
+export * from "./useGetLoginAuthSessions";
 export * from "./useGetLoginDevices";
 export * from "./useGetLogins";

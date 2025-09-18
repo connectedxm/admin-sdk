e35828b1bbc22f1f3cc282f34e45c1b0bb15c47a
rob upchurch <rob@connected.events>
2025-09-17 19:44:38 -0600

    Remove useConnectSeriesTemplate mutation and its export from series index for code cleanup.

--- src/mutations/series/index.ts
+++ src/mutations/series/index.ts
@@ -1,5 +1,4 @@
 export * from "./useAddSeriesEvent";
-export * from "./useConnectSeriesTemplate";
 export * from "./useCreateSeries";
 export * from "./useDeleteSeries";
 export * from "./useRemoveSeriesEvent";

f2eac027404d2782b1c415ccf2ba21856f50e6a1
rob upchurch <rob@connected.events>
2026-01-21 00:03:53 -0700

    Update version to 6.5.13 in package.json and package-lock.json; add meetingId to BaseActivity and Activity interfaces, and include activityId in MeetingCreateInputs and MeetingUpdateInputs interfaces.

--- src/params.ts
+++ src/params.ts
@@ -165,6 +165,7 @@ export interface ActivityCreateInputs {
   groupId?: string | null;
   contentId?: string | null;
   commentedId?: string | null;
+  meetingId?: string | null;
   createdAt?: string | null;
 }
 
@@ -177,6 +178,7 @@ export interface ActivityUpdateInputs {
   pinned?: boolean;
   imageId?: string | null;
   videoId?: string | null;
+  meetingId?: string | null;
   createdAt?: string | null;
 }
 
@@ -2463,6 +2465,7 @@ export interface MeetingCreateInputs {
   eventId?: string;
   sessionId?: string;
   groupId?: string;
+  activityId?: string;
   title: string | null;
   preferred_region:
     | "ap-south-1"
@@ -2518,6 +2521,7 @@ export interface MeetingUpdateInputs {
   eventId?: string;
   sessionId?: string;
   groupId?: string;
+  activityId?: string;
   title?: string | null;
   preferred_region?:
     | "ap-south-1"

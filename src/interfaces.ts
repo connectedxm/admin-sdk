833139643652bfcc16ad3bec36dd5a574f5be518
Spencer Fund <fund.spencer@gmail.com>
2026-03-05 15:14:23 -0700

    style: Consolidate interface extension declarations to single lines.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1826,8 +1826,7 @@ export interface BaseOrganizationModuleSettings {
   organizationId: string;
 }
 
-export interface OrganizationModuleSettings
-  extends BaseOrganizationModuleSettings {
+export interface OrganizationModuleSettings extends BaseOrganizationModuleSettings {
   meetingGroupCallAdminPreset: string;
   meetingGroupCallGuestPreset: string;
   meetingWebinarAdminPreset: string;
@@ -1843,8 +1842,7 @@ export interface BaseOrganizationModuleSettingsTranslation {
   locale: string;
 }
 
-export interface OrganizationModuleSettingsTranslation
-  extends BaseOrganizationModuleSettingsTranslation {
+export interface OrganizationModuleSettingsTranslation extends BaseOrganizationModuleSettingsTranslation {
   supportAutoResolveMessage?: string;
 }
 
@@ -2166,8 +2164,7 @@ export interface Question {
   value: string;
 }
 
-export interface RegistrationQuestionChoice
-  extends BaseRegistrationQuestionChoice {
+export interface RegistrationQuestionChoice extends BaseRegistrationQuestionChoice {
   questionId: string;
   question: BaseRegistrationQuestion;
   subQuestions: BaseRegistrationQuestionChoiceSubQuestion[];
@@ -2182,8 +2179,7 @@ export interface BaseRegistrationQuestionChoiceSubQuestion {
   question: BaseRegistrationQuestion;
 }
 
-export interface RegistrationQuestionChoiceSubQuestion
-  extends BaseRegistrationQuestionChoiceSubQuestion {
+export interface RegistrationQuestionChoiceSubQuestion extends BaseRegistrationQuestionChoiceSubQuestion {
   sortOrder: number;
   createdAt: string;
   updatedAt: string;
@@ -2210,8 +2206,7 @@ export interface BaseRegistrationQuestionResponseChange {
   createdAt: string;
 }
 
-export interface RegistrationQuestionResponseChange
-  extends BaseRegistrationQuestionResponseChange {
+export interface RegistrationQuestionResponseChange extends BaseRegistrationQuestionResponseChange {
   response: BaseRegistrationQuestionResponse;
   user: BaseUser | null;
 }
@@ -2223,8 +2218,7 @@ export interface BaseRegistrationQuestionResponse {
   question: BaseRegistrationQuestion;
 }
 
-export interface RegistrationQuestionResponse
-  extends BaseRegistrationQuestionResponse {
+export interface RegistrationQuestionResponse extends BaseRegistrationQuestionResponse {
   changeLogs: BaseRegistrationQuestionResponseChange[];
   createdAt: string;
   updatedAt: string;
@@ -2283,8 +2277,7 @@ export interface BaseRegistrationSectionQuestion {
   sortOrder: number;
 }
 
-export interface RegistrationSectionQuestion
-  extends BaseRegistrationSectionQuestion {
+export interface RegistrationSectionQuestion extends BaseRegistrationSectionQuestion {
   createdAt: string;
   updatedAt: string;
 }
@@ -2297,8 +2290,7 @@ export interface BaseRegistrationFollowupQuestion {
   sortOrder: number;
 }
 
-export interface RegistrationFollowupQuestion
-  extends BaseRegistrationFollowupQuestion {
+export interface RegistrationFollowupQuestion extends BaseRegistrationFollowupQuestion {
   createdAt: string;
   updatedAt: string;
 }
@@ -2565,8 +2557,7 @@ export interface BaseSeriesRegistrationQuestionResponse {
   question: BaseSeriesQuestion;
 }
 
-export interface SeriesRegistrationQuestionResponse
-  extends BaseSeriesRegistrationQuestionResponse {
+export interface SeriesRegistrationQuestionResponse extends BaseSeriesRegistrationQuestionResponse {
   fileId: number | null;
   file: BaseFile | null;
   createdAt: string;
@@ -2772,8 +2763,7 @@ export interface BaseEventSessionQuestionChoice {
   };
 }
 
-export interface EventSessionQuestionChoice
-  extends BaseEventSessionQuestionChoice {
+export interface EventSessionQuestionChoice extends BaseEventSessionQuestionChoice {
   questionId: string;
   question: BaseEventSessionQuestion;
   subQuestions: BaseEventSessionQuestionChoiceSubQuestion[];
@@ -2788,8 +2778,7 @@ export interface BaseEventSessionQuestionChoiceSubQuestion {
   question: BaseEventSessionQuestion;
 }
 
-export interface EventSessionQuestionChoiceSubQuestion
-  extends BaseEventSessionQuestionChoiceSubQuestion {
+export interface EventSessionQuestionChoiceSubQuestion extends BaseEventSessionQuestionChoiceSubQuestion {
   sortOrder: number;
   createdAt: string;
   updatedAt: string;
@@ -2816,8 +2805,7 @@ export interface BaseEventSessionQuestionResponseChange {
   createdAt: string;
 }
 
-export interface EventSessionQuestionResponseChange
-  extends BaseEventSessionQuestionResponseChange {
+export interface EventSessionQuestionResponseChange extends BaseEventSessionQuestionResponseChange {
   response: BaseEventSessionQuestionResponse;
   user: BaseUser;
 }
@@ -2829,8 +2817,7 @@ export interface BaseEventSessionQuestionResponse {
   question: BaseEventSessionQuestion;
 }
 
-export interface EventSessionQuestionResponse
-  extends BaseEventSessionQuestionResponse {
+export interface EventSessionQuestionResponse extends BaseEventSessionQuestionResponse {
   changeLogs: BaseEventSessionQuestionResponseChange[];
   createdAt: string;
   updatedAt: string;
@@ -2891,8 +2878,7 @@ export interface BaseEventSessionSectionQuestion {
   sortOrder: number;
 }
 
-export interface EventSessionSectionQuestion
-  extends BaseEventSessionSectionQuestion {
+export interface EventSessionSectionQuestion extends BaseEventSessionSectionQuestion {
   createdAt: string;
   updatedAt: string;
 }
@@ -3121,8 +3107,7 @@ export interface BaseMeetingSessionParticipant {
   account: BaseAccount | null;
 }
 
-export interface MeetingSessionParticipant
-  extends BaseMeetingSessionParticipant {
+export interface MeetingSessionParticipant extends BaseMeetingSessionParticipant {
   peer_stats?: {
     config?: string;
     status?: string;
@@ -3593,8 +3578,7 @@ export interface BaseEventPassTypePriceSchedule {
   updatedAt: string;
 }
 
-export interface EventPassTypePriceSchedule
-  extends BaseEventPassTypePriceSchedule {}
+export interface EventPassTypePriceSchedule extends BaseEventPassTypePriceSchedule {}
 
 export interface BaseEventPassTypeRefundSchedule {
   id: string;
@@ -3606,8 +3590,7 @@ export interface BaseEventPassTypeRefundSchedule {
   updatedAt: string;
 }
 
-export interface EventPassTypeRefundSchedule
-  extends BaseEventPassTypeRefundSchedule {}
+export interface EventPassTypeRefundSchedule extends BaseEventPassTypeRefundSchedule {}
 
 export interface EventPassTypeTranslation {
   id: number;
@@ -4062,8 +4045,10 @@ export interface BaseEventRoomTypeReservation {
   room: BaseRoom;
 }
 
-export interface EventRoomTypeReservation
-  extends Omit<BaseEventRoomTypeReservation, "passes"> {
+export interface EventRoomTypeReservation extends Omit<
+  BaseEventRoomTypeReservation,
+  "passes"
+> {
   passes: {
     id: string;
     status: PurchaseStatus;
@@ -4102,8 +4087,7 @@ export interface BaseEventRoomTypePassTypeDetails {
   maxEnd: string | null;
 }
 
-export interface EventRoomTypePassTypeDetails
-  extends BaseEventRoomTypePassTypeDetails {
+export interface EventRoomTypePassTypeDetails extends BaseEventRoomTypePassTypeDetails {
   createdAt: string;
   updatedAt: string;
 }
@@ -4119,8 +4103,7 @@ export interface BaseEventRoomTypeAddOnDetails {
   maxEnd: string | null;
 }
 
-export interface EventRoomTypeAddOnDetails
-  extends BaseEventRoomTypeAddOnDetails {
+export interface EventRoomTypeAddOnDetails extends BaseEventRoomTypeAddOnDetails {
   createdAt: string;
   updatedAt: string;
 }
@@ -4562,8 +4545,7 @@ export interface BaseSurveyQuestionChoiceSubQuestion {
   question: BaseSurveyQuestion;
 }
 
-export interface SurveyQuestionChoiceSubQuestion
-  extends BaseSurveyQuestionChoiceSubQuestion {
+export interface SurveyQuestionChoiceSubQuestion extends BaseSurveyQuestionChoiceSubQuestion {
   sortOrder: number;
   createdAt: string;
   updatedAt: string;
@@ -4590,8 +4572,7 @@ export interface BaseSurveyQuestionResponseChange {
   createdAt: string;
 }
 
-export interface SurveyQuestionResponseChange
-  extends BaseSurveyQuestionResponseChange {
+export interface SurveyQuestionResponseChange extends BaseSurveyQuestionResponseChange {
   response: BaseSurveyQuestionResponse;
   user: BaseUser;
 }
@@ -4992,8 +4973,7 @@ export interface BaseStreamSessionSubscription {
   disconnectedAt: string | null;
 }
 
-export interface StreamSessionSubscription
-  extends BaseStreamSessionSubscription {
+export interface StreamSessionSubscription extends BaseStreamSessionSubscription {
   connection: BaseWebSocketConnection;
   streamSession: BaseStreamSession;
 }

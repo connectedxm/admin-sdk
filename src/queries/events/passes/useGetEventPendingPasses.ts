75429b3c2824227b01602f2d71072ca5487ac2d7
Spencer Fund <fund.spencer@gmail.com>
2026-03-25 14:52:07 -0600

    refactor: simplify EVENT_PENDING_PASSES_QUERY_KEY and remove checkedIn parameter
    - Streamlined EVENT_PENDING_PASSES_QUERY_KEY by removing the checkedIn parameter.
    - Updated GetEventPendingPasses and useGetEventPendingPasses to reflect this change, ensuring cleaner query management.

--- src/queries/events/passes/useGetEventPendingPasses.ts
+++ src/queries/events/passes/useGetEventPendingPasses.ts
@@ -11,20 +11,12 @@ import { EVENT_QUERY_KEY } from "../useGetEvent";
  * @category Keys
  * @group Events
  */
-export const EVENT_PENDING_PASSES_QUERY_KEY = (
-  eventId: string,
-  checkedIn?: boolean
-) => {
-  const key = [...EVENT_QUERY_KEY(eventId), "PASSES", "PENDING"];
-  if (typeof checkedIn === "boolean") {
-    key.push(checkedIn ? "CHECKED_IN" : "NOT_CHECKED");
-  }
-  return key;
+export const EVENT_PENDING_PASSES_QUERY_KEY = (eventId: string) => {
+  return [...EVENT_QUERY_KEY(eventId), "PASSES", "PENDING"];
 };
 
 interface GetEventPendingPassesProps extends InfiniteQueryParams {
   eventId: string;
-  checkedIn?: boolean;
 }
 
 /**
@@ -33,7 +25,6 @@ interface GetEventPendingPassesProps extends InfiniteQueryParams {
  */
 export const GetEventPendingPasses = async ({
   eventId,
-  checkedIn,
   pageParam,
   pageSize,
   orderBy,
@@ -43,7 +34,6 @@ export const GetEventPendingPasses = async ({
   const adminApi = await GetAdminAPI(adminApiParams);
   const { data } = await adminApi.get(`/events/${eventId}/passes/pending`, {
     params: {
-      checkedIn: typeof checkedIn !== "undefined" ? checkedIn : undefined,
       page: pageParam || undefined,
       pageSize: pageSize || undefined,
       orderBy: orderBy || undefined,
@@ -60,7 +50,6 @@ export const GetEventPendingPasses = async ({
  */
 export const useGetEventPendingPasses = (
   eventId: string = "",
-  checkedIn?: boolean,
   params: Omit<
     InfiniteQueryParams,
     "pageParam" | "queryClient" | "adminApiParams"
@@ -72,12 +61,11 @@ export const useGetEventPendingPasses = (
   return useConnectedInfiniteQuery<
     Awaited<ReturnType<typeof GetEventPendingPasses>>
   >(
-    EVENT_PENDING_PASSES_QUERY_KEY(eventId, checkedIn),
+    EVENT_PENDING_PASSES_QUERY_KEY(eventId),
     (params: InfiniteQueryParams) =>
       GetEventPendingPasses({
         ...params,
         eventId,
-        checkedIn,
       }),
     params,
     {
